23a4714be68de0956dce19ab2f7f05f45af8f99a
Saad Saiyed <saadsaiyed7@gmail.com>
2021-07-22 19:54:32 -0400

    Working with git commits

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'Hello': 'World' });
-});
+passport.use(
+    new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, (accessToken, refreshToken, profile, done) => {
+            console.log('Access Token',accessToken);
+            console.log('Refresh Token', refreshToken);
+            console.log('Profile', profile);
+        }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate(
+        'google',
+        { scope: ['profile', 'email'] }
+    )
+);
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
